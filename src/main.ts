18b5a5c17e5960aa5d70d08481506d39e9edf167
Trương Thanh Tâm <tamtruong037@gmail.com>
2020-12-23 21:32:11 +0700

    commit all

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 import { CoreStoreModule } from './core/core-store.module';
 const logger = new Logger('Bootstrap');
-import 'dotenv/config';
+// import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  // app.setGlobalPrefix('aa');
   useContainer(app.select(CoreStoreModule), { fallbackOnErrors: true });
   await app.listenAsync(process.env.PORT_APP);
 }
